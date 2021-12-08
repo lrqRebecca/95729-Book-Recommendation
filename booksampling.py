f3a37942e643f2f18842dea845d55ab682df0ab9
lrqRebecca <994042899@qq.com>
2021-12-08 10:08:13 -0500

    Initial sampling of books
    This module is to randomly sample 10 books for the user to rate.

---
+++ booksampling.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 28 23:25:45 2021
+
+@author: ruoqili
+"""
+import pandas as pd
+
+'''
+    Randomly sample 10 books from the begining list of books 
+'''
+
+def get_sampling():
+    book_list = pd.read_csv("Begin_book_list.csv")
+    return book_list.sample(n=10, replace=False).to_dict('records')
